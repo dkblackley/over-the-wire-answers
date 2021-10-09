ee179f07d1a6d862a8192be4ebb0d637154d3cca
dkblackley <dkblackley@dundee.ac.uk>
2021-10-09 09:52:44 +0100

    added initial script

---
+++ mk_dirs_and_scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+folders=1
+games=("Bandit Natas Leviathan Krypton Narnia Behemoth Utumno Maze Vortex Semtex Manpage Drifter")
+levels=(34 34 7 7 9 8 8 9 27 12 7 15)
+
+echo $levels
+
+for folder in $games
+do
+    echo "Welcome $folder times"
+done
